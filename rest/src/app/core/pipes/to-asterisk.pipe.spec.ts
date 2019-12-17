43035cfdf0793391de44dc105f49b2aa86dfa905
Lea Drudi <drudilea@hotmail.com>
2019-12-17 20:35:15 -0300

    First commit

--- rest/src/app/core/pipes/to-asterisk.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToAsteriskPipe } from './to-asterisk.pipe';
-
-describe('ToAsteriskPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToAsteriskPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
