3a877af1948df8ea39d1f4c7b5cc332ca0495b7a
Leandro Drudi <>
2020-02-03 18:02:44 -0300

    Inputs and Outputs for components

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -12,9 +12,12 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./login/register.component";
 
+// Temporal
+import { FormsModule } from "@angular/forms";
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, APP_ROUTES, PagesModule],
+  imports: [BrowserModule, APP_ROUTES, PagesModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- adminpro/src/app/pages/pages.module.ts
+++ adminpro/src/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { SharedModule } from "../shared/shared.module";
 
@@ -7,15 +8,19 @@ import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
 import { PagesComponent } from "./pages.component";
 import { PAGES_ROUTES } from "./pages.routes";
+import { IncrementadorComponent } from "../components/incrementador/incrementador.component";
+
+// Temporal
 
 @NgModule({
   declarations: [
     DashboardComponent,
     ProgressComponent,
     Graficas1Component,
-    PagesComponent
+    PagesComponent,
+    IncrementadorComponent
   ],
   exports: [DashboardComponent, ProgressComponent, Graficas1Component],
-  imports: [SharedModule, PAGES_ROUTES]
+  imports: [SharedModule, PAGES_ROUTES, FormsModule]
 })
 export class PagesModule {}
