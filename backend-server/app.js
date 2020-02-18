96d986e8766522262db39542e16406c70f2fb73b
Leandro Drudi <drudilea@hotmail.com>
2020-02-18 01:49:28 -0300

    Inicializacion backend server

---
+++ backend-server/app.js
@@ -0,0 +1,31 @@
+// Requires (importacion de librerias)
+var express = require("express");
+var mongoose = require("mongoose");
+
+// Inicializar variables
+var app = express();
+
+// Conexion a la base de datos
+mongoose.connection.openUri(
+  "mongodb://localhost:27017/hospitalDB",
+  (err, res) => {
+    // En caso que suceda un error se detiene todo el proceso
+    if (err) throw err;
+
+    console.log("Base de datos  \x1b[32m%s\x1b[0m", "online");
+  }
+);
+
+// Rutas
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    ok: true,
+    mensaje: "Peticion realizada correctamente"
+  });
+});
+
+// Escuchar peticiones en un puerto determinado
+app.listen(3000, () => {
+  // Agrego color a la palabra para diferenciarlas
+  console.log("Express server puerto 3000: \x1b[32m%s\x1b[0m", "online");
+});
