9a9ae8a4ae567dd9b86c02d644a789a6c8aafa61
Lea Drudi <drudilea@hotmail.com>
2021-01-06 12:19:36 -0300

    Interceptor para Token implementado

--- adminpro/src/app/app.module.ts
+++ adminpro/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
 
 // Modulos
 import { PagesModule } from "./pages/pages.module";
@@ -17,8 +18,7 @@ import { NopagefoundComponent } from "./nopagefound/nopagefound.component";
 
 // Servicios
 import { ServiceModule } from "./services/service.module";
-
-// Temporal
+import { TokenInterceptorService } from "./interceptors/token-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent, NopagefoundComponent],
@@ -32,7 +32,13 @@ import { ServiceModule } from "./services/service.module";
     ServiceModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
